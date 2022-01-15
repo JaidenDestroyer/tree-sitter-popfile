fc96068a6d660afd82a3075884445ac08e902b0e
JaidenDestroyer <jaidendestroyer@gmail.com>
2022-01-14 20:09:25 -0500

    Added stuff

---
+++ grammar.js
@@ -0,0 +1,7 @@
+module.exports = grammar({
+    name: 'popfile',
+
+    rules: {
+
+    }
+  });
---
+++ src/parser.c
@@ -0,0 +1,187 @@
+#include <tree_sitter/parser.h>
+
+#if defined(__GNUC__) || defined(__clang__)
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wmissing-field-initializers"
+#endif
+
+#define LANGUAGE_VERSION 13
+#define STATE_COUNT 3
+#define LARGE_STATE_COUNT 2
+#define SYMBOL_COUNT 4
+#define ALIAS_COUNT 0
+#define TOKEN_COUNT 3
+#define EXTERNAL_TOKEN_COUNT 0
+#define FIELD_COUNT 0
+#define MAX_ALIAS_SEQUENCE_LENGTH 1
+#define PRODUCTION_ID_COUNT 1
+
+enum {
+  anon_sym_import = 1,
+  anon_sym_base = 2,
+  sym_import_statement = 3,
+};
+
+static const char * const ts_symbol_names[] = {
+  [ts_builtin_sym_end] = "end",
+  [anon_sym_import] = "import",
+  [anon_sym_base] = "base",
+  [sym_import_statement] = "import_statement",
+};
+
+static const TSSymbol ts_symbol_map[] = {
+  [ts_builtin_sym_end] = ts_builtin_sym_end,
+  [anon_sym_import] = anon_sym_import,
+  [anon_sym_base] = anon_sym_base,
+  [sym_import_statement] = sym_import_statement,
+};
+
+static const TSSymbolMetadata ts_symbol_metadata[] = {
+  [ts_builtin_sym_end] = {
+    .visible = false,
+    .named = true,
+  },
+  [anon_sym_import] = {
+    .visible = true,
+    .named = false,
+  },
+  [anon_sym_base] = {
+    .visible = true,
+    .named = false,
+  },
+  [sym_import_statement] = {
+    .visible = true,
+    .named = true,
+  },
+};
+
+static const TSSymbol ts_alias_sequences[PRODUCTION_ID_COUNT][MAX_ALIAS_SEQUENCE_LENGTH] = {
+  [0] = {0},
+};
+
+static const uint16_t ts_non_terminal_alias_map[] = {
+  0,
+};
+
+static bool ts_lex(TSLexer *lexer, TSStateId state) {
+  START_LEXER();
+  eof = lexer->eof(lexer);
+  switch (state) {
+    case 0:
+      if (eof) ADVANCE(9);
+      if (lookahead == 'b') ADVANCE(1);
+      if (lookahead == 'i') ADVANCE(3);
+      if (lookahead == '\t' ||
+          lookahead == '\n' ||
+          lookahead == '\r' ||
+          lookahead == ' ') SKIP(0)
+      END_STATE();
+    case 1:
+      if (lookahead == 'a') ADVANCE(7);
+      END_STATE();
+    case 2:
+      if (lookahead == 'e') ADVANCE(11);
+      END_STATE();
+    case 3:
+      if (lookahead == 'm') ADVANCE(5);
+      END_STATE();
+    case 4:
+      if (lookahead == 'o') ADVANCE(6);
+      END_STATE();
+    case 5:
+      if (lookahead == 'p') ADVANCE(4);
+      END_STATE();
+    case 6:
+      if (lookahead == 'r') ADVANCE(8);
+      END_STATE();
+    case 7:
+      if (lookahead == 's') ADVANCE(2);
+      END_STATE();
+    case 8:
+      if (lookahead == 't') ADVANCE(10);
+      END_STATE();
+    case 9:
+      ACCEPT_TOKEN(ts_builtin_sym_end);
+      END_STATE();
+    case 10:
+      ACCEPT_TOKEN(anon_sym_import);
+      END_STATE();
+    case 11:
+      ACCEPT_TOKEN(anon_sym_base);
+      END_STATE();
+    default:
+      return false;
+  }
+}
+
+static const TSLexMode ts_lex_modes[STATE_COUNT] = {
+  [0] = {.lex_state = 0},
+  [1] = {.lex_state = 0},
+  [2] = {.lex_state = 0},
+};
+
+static const uint16_t ts_parse_table[LARGE_STATE_COUNT][SYMBOL_COUNT] = {
+  [0] = {
+    [ts_builtin_sym_end] = ACTIONS(1),
+    [anon_sym_import] = ACTIONS(1),
+    [anon_sym_base] = ACTIONS(1),
+  },
+  [1] = {
+    [sym_import_statement] = STATE(2),
+    [ts_builtin_sym_end] = ACTIONS(3),
+  },
+};
+
+static const uint16_t ts_small_parse_table[] = {
+  [0] = 1,
+    ACTIONS(5), 1,
+      ts_builtin_sym_end,
+};
+
+static const uint32_t ts_small_parse_table_map[] = {
+  [SMALL_STATE(2)] = 0,
+};
+
+static const TSParseActionEntry ts_parse_actions[] = {
+  [0] = {.entry = {.count = 0, .reusable = false}},
+  [1] = {.entry = {.count = 1, .reusable = false}}, RECOVER(),
+  [3] = {.entry = {.count = 1, .reusable = true}}, REDUCE(sym_import_statement, 0),
+  [5] = {.entry = {.count = 1, .reusable = true}},  ACCEPT_INPUT(),
+};
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+#ifdef _WIN32
+#define extern __declspec(dllexport)
+#endif
+
+extern const TSLanguage *tree_sitter_popfile(void) {
+  static const TSLanguage language = {
+    .version = LANGUAGE_VERSION,
+    .symbol_count = SYMBOL_COUNT,
+    .alias_count = ALIAS_COUNT,
+    .token_count = TOKEN_COUNT,
+    .external_token_count = EXTERNAL_TOKEN_COUNT,
+    .state_count = STATE_COUNT,
+    .large_state_count = LARGE_STATE_COUNT,
+    .production_id_count = PRODUCTION_ID_COUNT,
+    .field_count = FIELD_COUNT,
+    .max_alias_sequence_length = MAX_ALIAS_SEQUENCE_LENGTH,
+    .parse_table = &ts_parse_table[0][0],
+    .small_parse_table = ts_small_parse_table,
+    .small_parse_table_map = ts_small_parse_table_map,
+    .parse_actions = ts_parse_actions,
+    .symbol_names = ts_symbol_names,
+    .symbol_metadata = ts_symbol_metadata,
+    .public_symbol_map = ts_symbol_map,
+    .alias_map = ts_non_terminal_alias_map,
+    .alias_sequences = &ts_alias_sequences[0][0],
+    .lex_modes = ts_lex_modes,
+    .lex_fn = ts_lex,
+  };
+  return &language;
+}
+#ifdef __cplusplus
+}
+#endif
